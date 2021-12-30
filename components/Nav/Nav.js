87422d08704e2538f823db2a3ee57e04c78e1db9
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-12-30 15:28:39 +0500

    Create navbar

---
+++ components/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div>
+      <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1">Navbar</span>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
