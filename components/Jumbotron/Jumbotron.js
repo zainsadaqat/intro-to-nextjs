cf035f5f8b9db1f50fe59637f2a460ddd204feb8
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-12-30 15:27:49 +0500

    Create Jumbotron

---
+++ components/Jumbotron/Jumbotron.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Jumbotron = () => {
+  return (
+    <div class="jumbotron jumbotron-fluid mt-5">
+      <div class="container">
+        <h1 class="display-4">Cats World</h1>
+        <p class="lead">Find your new best friend today.</p>
+        <button type="button" class="btn btn-primary">
+          Start Looking!
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Jumbotron;
