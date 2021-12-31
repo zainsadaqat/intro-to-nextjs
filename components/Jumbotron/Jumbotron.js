15d1d82f84124ea7280bc960f1c37b5e6406bff9
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-12-31 08:13:30 +0500

    Create jumbotron

--- components/Jumbotron/Jumbotron.js
+++ components/Jumbotron/Jumbotron.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 const Jumbotron = () => {
+  const router = useRouter();
   return (
-    <div class="jumbotron jumbotron-fluid mt-5">
-      <div class="container">
-        <h1 class="display-4">Cats World</h1>
-        <p class="lead">Find your new best friend today.</p>
-        <button type="button" class="btn btn-primary">
+    <div className="jumbotron jumbotron-fluid mt-5">
+      <div className="container">
+        <h1 className="display-4">Cats World</h1>
+        <p className="lead">Find your new best friend today.</p>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => router.push('/cats')}
+        >
           Start Looking!
         </button>
       </div>
