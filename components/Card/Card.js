200e5e5c1522662e9327d152f964cf77e6f7d734
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-12-31 08:13:21 +0500

    Create card comp

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import styles from './Card.module.css';
 
-const Card = ({ name, phone, email, image }) => {
+const Card = ({ name, phone, email, image, id }) => {
+  const router = useRouter();
   return (
-    <div className={styles.card}>
+    <div className={styles.card} onClick={() => router.push(`/cats/${id}`)}>
       <div className={styles['card-header']}>
-        <img src={image.url} alt={image.alt} className={styles['card-image']} />
+        <img src={image.url} alt={name} className={styles['card-img']} />
       </div>
       <div>
         <h3>{name}</h3>
