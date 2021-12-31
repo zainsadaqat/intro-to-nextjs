817d90de9a3d21810abe811a7573c3f79c5cf019
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-12-31 08:14:52 +0500

    Frontend all cats

--- pages/cats/index.js
+++ pages/cats/index.js
@@ -1,14 +1,46 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import Card from '../../components/Card/Card';
-import Nav from '../../components/Nav/Nav';
+import DefaultLayout from '../../layouts/Default';
 
-const cats = () => {
+const index = () => {
+  const [cats, setCats] = useState([]);
+  const fetchCats = async () => {
+    const response = await fetch('/api/cats');
+    const data = await response.json();
+    setCats(data);
+  };
+
+  useEffect(() => {
+    fetchCats();
+  }, []);
+
   return (
-    <div>
-      <Nav />
-      <Card />
-    </div>
+    <>
+      <DefaultLayout>
+        <div className="container mt-5">
+          <div
+            style={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              justifyContent: 'center',
+              gap: 30,
+            }}
+          >
+            {cats.map((cat) => (
+              <Card
+                name={cat.name}
+                email={cat.email}
+                phone={cat.phone}
+                id={cat.id}
+                image={cat.image}
+              />
+            ))}
+          </div>
+        </div>
+      </DefaultLayout>
+    </>
   );
 };
 
-export default cats;
+export default index;
